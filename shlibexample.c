28efed651773d7b56bf055026fe75a6070a3aa40
A Path in the Ordinary <36130385+hqh2010@users.noreply.github.com>
2024-02-27 17:15:25 +0800

    Update shlibexample.c

--- shlibexample.c
+++ shlibexample.c
@@ -2,6 +2,38 @@
 #include <stdio.h>
 #include "shlibexample.h"
 
+/**  可以通过标准C库函数获取API对应的so文件的绝对路径
+#include <cstdlib>
+#include <dlfcn.h>
+#include <iostream>
+
+std::string GetAbsolutePath(const std::string& fpath)
+{
+     char buffer[1024] = {'\0'};
+     if (realpath(fpath.c_str(), buffer) != nullptr)
+     {
+         std::cout << "get success" << std::endl;
+         return std::string(buffer);
+
+     }
+     return "";
+
+
+}
+
+void GetSharedLibPath()
+{
+    Dl_info dl_info;
+    if (dladdr((void*)GetSharedLibPath, &dl_info) != 0)
+    {
+         std::cout<< GetAbsolutePath(dl_info.dli_fname) << std::endl;  
+    } else
+    {
+         std::cout<< "can't find lib path" << std::endl;  
+    }
+}
+**/
+
 int SharedLibApi() {
 
    printf("this is a shared libary demo\n");
